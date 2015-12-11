030157f9509a559742b336934cbfab40876bdf38
Matthew Musich <mcr1221@hotmail.com>
2015-12-10 19:24:41 -0500

    cookie problem fixed

--- pubsite/js/reactNewEval.js
+++ pubsite/js/reactNewEval.js
@@ -3,11 +3,15 @@
  */
 //Load in the api to build the data object
 var newevaldata = new Object();
-
+var newevaldata2 = new Object();
+var studentid = 20;
+if( getCookie("studentid") != ""){
+   studentid = getCookie("studentid");
+}
 
 $(document).ready(function() {
     jQuery.ajax( {
-    url: "http://capping.xyz:3000/api/students/20/courses/",
+    url: "http://capping.xyz:3000/api/majors",
     type: "GET",
     crossDomain: true, 
     contentType: "text/plain; charset=utf-8",
@@ -16,7 +20,7 @@ $(document).ready(function() {
     success: function( response ) {
         //console.log(response);
         newevaldata = response;
-        
+        loadMajors()
     },
     xhrFields: {
     // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
@@ -30,14 +34,92 @@ $(document).ready(function() {
    
 });
 
+
+function loadMajors(){
+    var allmajors = [];
+    newevaldata.forEach( function (item){
+     var x = item.title;
+        allmajors.push(x);
+    });
+    //console.log(allmajors);
+    
+    var majors = [];
+    $.each(allmajors, function(i, el){
+    if($.inArray(el, majors) === -1) majors.push(el);
+});
+    
+    var options = '';
+  for(var i = 0; i < majors.length; i++){
+    options += '<option value="'+majors[i]+'" />'+majors[i]+'</option>';
+  }
+    
+  document.getElementById('major').innerHTML = options;
+}
+
+function getMajorid(){
+    var pickedMajorid = 0;
+    var pickedMajorTitle = document.getElementById('major').value;
+    //console.log(newevaldata);
+        newevaldata.forEach( function (item){
+                if(item.title == pickedMajorTitle){
+                    pickedMajorid = item.mid;
+                    //console.log(item.mid);
+                }
+            });
+    //console.log(newevaldata);
+    //console.log(pickedMajorTitle + " " + pickedMajorid);
+    return pickedMajorid;
+}
+
+function runEval(){
+    var majorid = getMajorid();
+    //console.log(majorid + " " + studentid);
+
+    jQuery.ajax( {
+    url: "http://capping.xyz:3000/api/equivalencies/majors/"+ majorid +"/students/" + studentid + "",
+    type: "GET",
+    crossDomain: true, 
+    contentType: "text/plain; charset=utf-8",
+    //data: studentobj,
+    //dataType: "json",
+    async: false,
+    success: function( response ) {
+        //console.log(response);
+       newevaldata2 = response;
+        updateReact();
+        
+        
+    },
+    error: function(textStatus) {
+        console.log(textStatus);
+    },
+    
+    xhrFields: {
+    // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+    // This can be used to set the 'withCredentials' property.
+    // Set the value to 'true' if you'd like to pass cookies to the server.
+    // If this is enabled, your server must respond with the header
+    // 'Access-Control-Allow-Credentials: true'.
+        withCredentials: false
+    }
+});  
+    
+}
+
+
+
+
+
+function updateReact(){
+
 var CourseRow = React.createClass({
     render: function() {
         return (
             <tr>
-                <td>{this.props.courses.subject}</td>
-                <td>{this.props.courses.coursenum}</td>
-                <td>{this.props.courses.coursetitle}</td>
-                <td>{this.props.courses.coursenum}</td>
+                <td>{this.props.courses.studentCourseSubject} {this.props.courses.studentCourseNumber}  {this.props.courses.studentCourseName}</td>
+                <td>{this.props.courses.doesTransfer.toString()}</td>
+                <td>{this.props.courses.courseSubject} {this.props.courses.courseNumber} {this.props.courses.courseName}</td>
+                <td>{this.props.courses.credits}</td>
             </tr>
         );
     }
@@ -47,7 +129,7 @@ var CourseTable = React.createClass({
     render: function() {
         var rows = [];
         this.props.courses.forEach(function(courses) {
-            rows.push(<CourseRow courses={courses} key={courses.cid} />);
+            rows.push(<CourseRow courses={courses} key={courses.studentCourseId} />);
         }.bind(this));
         return (
             
@@ -57,7 +139,7 @@ var CourseTable = React.createClass({
               <tr>
                 <th>DCC Course</th>
                 <th>Does it Transfer</th>
-                <th>Marist Coruse</th>
+                <th>Marist Course</th>
                 <th>Credits</th>
               </tr>
             </thead>
@@ -68,10 +150,6 @@ var CourseTable = React.createClass({
     }
 });
         
-var buttonStyle = {
-  float: 'right'
-};
-
 
 
 var AllCourseTable = React.createClass({
@@ -87,7 +165,8 @@ var AllCourseTable = React.createClass({
 
 
 React.render(
-    <AllCourseTable courses={newevaldata} />,
+    <AllCourseTable courses={newevaldata2} />,
     document.getElementById('reactNewEval')
 );
 
+}
--- pubsite/js/reactSignIn.js
+++ pubsite/js/reactSignIn.js
@@ -44,3 +44,12 @@ function loginUser() {
 });  
     
 }
+
+$(document).ready(function(){
+    deleteCookie("studentid");
+    window.location.replace("http://www.capping.xyz/login.html");
+});
+
+var deleteCookie = function(name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
